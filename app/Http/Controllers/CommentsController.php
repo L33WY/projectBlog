3bffca02b57d454889dc8b228256d896cb9085c1
Dawid Lewandowski <l33wy@wp.pl>
2021-06-14 14:10:55 +0200

    added middleware before adding comments

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -9,6 +9,12 @@ use Illuminate\Support\Facades\Auth;
 
 class CommentsController extends Controller
 {
+        public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
     public function create(Post $post)
     {
         return view('comments/create', compact('post'));
@@ -16,6 +22,7 @@ class CommentsController extends Controller
 
     public function store()
     {
+
         $data = request()->validate([
            'description' => 'required|max:200',
             'post_id' => ''
